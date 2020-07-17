823f2ffaa80db78c2f4512a39bc3332be49296a1
watronfire <snowboardman007@gmail.com>
2020-07-17 13:51:19 -0700

    Rewrote main() and simulate_reads() to be more portable.

--- simulate_reads.py
+++ simulate_reads.py
@@ -1,7 +1,7 @@
 import argparse
 import numpy as np
 import tempfile
-from subprocess import call
+from subprocess import call, STDOUT
 import os
 from Bio import SeqIO
 import pandas as pd
@@ -52,7 +52,7 @@ def get_coverage( primer_scheme, reads, coverage ):
     Iterable
         a list of type:int which correspond to the number of reads to be generated for each amplicon.
     """
-    if coverage:
+    if coverage is not None:
         return np.int_( np.random.normal( reads, reads * 0.05, len( primer_scheme ) ) )
     else:
         distribution = np.ones( len( primer_scheme ) ) / len( primer_scheme )
@@ -186,7 +186,8 @@ def art_wrapper( executable, temp_location, reference, reads, read_length, seque
     if not verbose:
         command += " -q"
 
-    call( command, shell=True )
+    with open(os.devnull, 'w') as FNULL:
+        call( command, stdout=FNULL, shell=True )
 
     # Remove the amplicon fasta. Shouldn't have to use after this.
     os.remove( reference )
@@ -212,7 +213,7 @@ def combine_and_output( amplicon_locations, output_prefix ):
             with open( sim_pair[1], "r" ) as second:
                 for line in second: second_read.write( line )
 
-def generate_variants( reference, count=None, count_exp=10, frequency_scale=0.09, var_type="all", retry_limit=1000 ):
+def generate_variants( reference, count=None, count_exp=None, frequency_scale=None, var_type="all", retry_limit=1000 ):
     """ Generates a simulated vcf file to use for use in simulating variants. Will be expanded to with optional manual \
     functionality. Currently samples variant frequency from an inverse exponential distribution.
     Parameters
@@ -235,33 +236,43 @@ def generate_variants( reference, count=None, count_exp=10, frequency_scale=0.09
     pd.Dataframe
         Table in Variant Call Format containing simulated variants.
     """
+    # Calculate the number of variants to generate. If not specified draw from a poisson distribution centered around county_exp.
     if count is not None:
         variants = count
     else:
-        variants = np.random.poisson( count_exp )
+        variants = np.random.poisson( count_exp ) if count_exp is not None else np.random.poisson( 10 )
 
+    # Intialize variant requirements depending on the variant type requested.
+    # TODO determine an adequate frequency scale for generating major/all variants.
     if var_type == "minor":
         frequency_requirement = lambda x: x < 0.5
+        if frequency_scale is None:
+            frequency_scale = 0.09
     elif var_type == "major":
         frequency_requirement = lambda x : 1 > x > 0.5
+        if frequency_scale is None:
+            frequency_scale = 0.12
     elif var_type == "all":
         frequency_requirement = lambda x : True
+        if frequency_scale is None:
+            frequency_scale = 0.1
     else:
         sys.exit( "'{}' is not a known variant type. Valid options are ['minor', 'major', 'all']".format( var_type ) )
 
     var_table = list()
 
-    ref_length = len( reference )
-
+    # For each variant required call a random base to modify, a random substitution, and a random frequency.
     for i in range( variants ):
         bases = ["A", "T", "C", "G"]
 
-        base_to_modify = np.random.randint( 0, ref_length )
+        base_to_modify = np.random.randint( 0, len( reference ) )
         ancestral = reference.seq[base_to_modify]
         bases.remove( ancestral )
         minor_variant = np.random.choice( bases )
         alternative_frequency = np.random.exponential( scale=frequency_scale )
 
+        # The frequency samplier isn't perfect, so we draw from it until we get a reasonable frequency. If this doesn't \
+        # happen, it means you're either really unlucky or the frequency scale is off.
         retry_count = 0
         while not frequency_requirement( alternative_frequency ):
             alternative_frequency = np.random.exponential( scale=frequency_scale )
@@ -271,13 +282,76 @@ def generate_variants( reference, count=None, count_exp=10, frequency_scale=0.09
 
         var_table.append( ( reference.name, base_to_modify, ancestral, minor_variant, alternative_frequency ) )
 
+    # Return a pseudo-VCF table.
     return_df = pd.DataFrame( var_table, columns=["CHROM", "POS", "REF", "ALT", "ALT_FREQ"] )
 
 
 
     return pd.DataFrame( var_table, columns=["CHROM", "POS", "REF", "ALT", "ALT_FREQ"] )
 
-def simulate_reads( executable, primer_pairs, variants_table, ref, coverage, output, seed=None ):
+def simulate_reads( executable, primers, ref, output, coverage=None, variants=False, variant_count=None,
+                    variant_exp=None, variants_table=None, seed=None, reads=None, frequency_scale=None, var_type="all",
+                    retry_limit=1000 ):
+    """
+    Parameters
+    ----------
+    executable : str
+        Location of the ART executable if not in path.
+    primers : str
+        The location of the primer bed file
+    ref : Bio.Seq.SeqRecord
+        The reference sequence from which to simulate variants on.
+    output : str
+        Prefix to append to output file.
+    coverage : int
+        Mean number of reads to generate per amplicon.
+    variants : bool
+        Whether to generate variants or not.
+    variant_count : int
+        Number of variants to generate.
+    variant_exp : int
+        Expected number of variants to generate. Used when variant_count not specified and the number of variants is \
+        drawn from a poisson distribution.
+    variants_table : pandas.DataFrame
+        Specify the variants to generate. Will override variants, variant_count, and variant_exp.
+    seed : int
+        The random seed to use for the simulation.
+    reads : int
+        The total number of reads to generate. If specified, will override coverage value.
+    frequency_scale : int, optional
+        The scale parameter to use to parameterize the exponential distribution. Higher value will induce higher frequencies.
+    var_type : {'all', 'major', 'minor'}
+        Indicates what type of variants to generate. Useable options are: "minor" - alternative frequency < 0.5, "major" - alternative frequency > 0.5, "all" - alternative frequency < 1.
+    retry_limit : int, optional
+        The number of tries the sampler can used to generate a suitable alternative frequency according to var_type.
+
+    Returns
+    -------
+    None
+        paired fastq files will be generated with output prefix.
+    """
+    # Initialize random seed if request.
+    if seed is not None:
+        np.random.seed( seed )
+    else:
+        # My plan is to print this at the end so you can rerun, but I don't know if that'll do what I want.
+        # TODO: test seed saving ability.
+        seed_used = np.random.get_state()
+
+    primer_pairs = parse_primer_scheme( primers )
+
+    if reads is None:
+        amplicon_cov = get_coverage( primer_pairs, reads=coverage, coverage=True )
+    else:
+        amplicon_cov =  get_coverage( primer_pairs, reads=reads, coverage=False )
+
+    if variants:
+        if variants_table is None:
+            variants_table = generate_variants( ref, count=variant_count, count_exp=variant_exp,
+                                                frequency_scale=frequency_scale, var_type=var_type,
+                                                retry_limit=retry_limit )
+        variants_table.to_csv( output + ".vcf", sep="\t", index=False )
+
     return_list = list()
     with tempfile.TemporaryDirectory() as tmpdirname:
         print( "created temporary directory", tmpdirname )
@@ -286,7 +360,7 @@ def simulate_reads( executable, primer_pairs, variants_table, ref, coverage, out
                                    reference=ref,
                                    primer_pair=pair,
                                    variants=variants_table,
-                                   reads_assigned=coverage[i] )
+                                   reads_assigned=amplicon_cov[i] )
             for j in af:
                 return_list.append( art_wrapper( executable=executable,
                                                  temp_location=tmpdirname,
@@ -296,35 +370,27 @@ def simulate_reads( executable, primer_pairs, variants_table, ref, coverage, out
                                                  seed=seed) )
         combine_and_output( return_list, output )
 
+# TODO: Move some of the functionality of this into simulate reads. Right now it is difficult to use the script as an
+#  import.
 def main( arguments ):
+    """
+    Parameters
+    ----------
+    arguments : Namespace
+        Output from the argparse.parse_args() function.
 
-    if arguments.seed:
-        np.random.seed( arguments.seed )
-
-    primer_pairs = parse_primer_scheme( arguments.primers )
-
+    Returns
+    -------
+    None
+        paired fastq files will be generated with output prefix.
+    """
     ref = SeqIO.read( arguments.reference, "fasta" )
 
-    # Generate variant file:
-    variants_table = None
-    if arguments.variants:
-        if arguments.variants_count:
-            variants_table = generate_variants( ref, count=arguments.variants_count )
-        elif arguments.variants_std:
-            variants_table = generate_variants( ref, count_exp=arguments.variants_std )
-        else:
-            variants_table = generate_variants( ref )
-        #if arguments.correct:
-        #    variants_table = correct_variants( variants_table )
-
-    print( variants_table.sort_values( "POS" ) )
-
-    if arguments.reads:
-        amplicon_coverage = get_coverage( primer_pairs, arguments.reads, False )
-    else:
-        amplicon_coverage = get_coverage( primer_pairs, arguments.coverage, True )
-
-    simulate_reads( args.art, primer_pairs, variants_table, ref, amplicon_coverage, arguments.output, seed=arguments.seed )
+    simulate_reads( executable=arguments.art, primers=arguments.primers, ref=ref, output=arguments.output,
+                    coverage=arguments.coverage, reads=arguments.reads, variants=arguments.variants,
+                    variant_count=arguments.variants_count, variant_exp=arguments.variants_std,
+                    seed=arguments.seed, frequency_scale=arguments.frequency_scale, var_type=arguments.variant_type,
+                    retry_limit=arguments.limit )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser( description="" )
@@ -341,6 +407,9 @@ if __name__ == "__main__":
     parser.add_argument( "-vc", "--variants-count", type=int, help="number of variants to simulated" )
     parser.add_argument( "-vs", "--variants-std", type=int, help="specify the expectation of number of variants to simulated from a poisson distribution" )
     parser.add_argument( "-rs", "--seed", type=int, help="random seed to reproduce results" )
+    parser.add_argument( "-fs", "--frequency-scale", type=float, help="scale parameter for the exponential distribution used for variant generation" )
+    parser.add_argument( "-vt", "--variant-type", type=str, choices=["all", "major", "minor"], default="all", help="type of variant to generate; restricts variant frequency to specific range." )
+    parser.add_argument( "-l", "--limit", type=int, default=1000, help="limit of the number of tries the variant sampler gets." )
 
     # Determine coverage
     coverage_type = parser.add_mutually_exclusive_group( required=True )
